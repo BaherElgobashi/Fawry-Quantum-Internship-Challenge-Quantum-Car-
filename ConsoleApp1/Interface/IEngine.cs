70085f1a5b086e08f8327150c3ef4eb372a4eefa
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-04-24 15:36:38 +0300

    Add project files.

---
+++ ConsoleApp1/Interface/IEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Interface
+{
+    public interface IEngine
+    {
+        // Car has four operations 
+        void start();
+        void stop();
+        void Accelerate();
+        void Decrease();
+
+        ///////////////////////////////////////////////////////////////////////////////
+
+        int GetSpeed();
+        void SetSpeed(int speed);
+    }
+}
