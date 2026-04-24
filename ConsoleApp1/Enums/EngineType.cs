fdabb08290b2277cd6ce1c287025e85fb5737288
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-04-24 16:45:00 +0300

    Add EngineType as an Enum to Choose the Specific Type.

---
+++ ConsoleApp1/Enums/EngineType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Enums
+{
+    public enum EngineType
+    {
+        GasEngine,
+        ElectricEngine,
+        HybridEngine,
+    }
+}
