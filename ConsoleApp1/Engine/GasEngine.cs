3fbfa0f88c2ffdb470f68f23a40f9b99d645c7a1
BaherElgobashi <bahermohamedelgobashi@gmail.com>
2026-04-24 15:44:17 +0300

    Implement the IEngine Interface inside the GasEngine Class.

---
+++ ConsoleApp1/Engine/GasEngine.cs
@@ -0,0 +1,47 @@
+﻿using ConsoleApp1.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Engine
+{
+    public class GasEngine : IEngine
+    {
+        private int _speed = 0;
+
+        public void start()
+        {
+            _speed = 0;
+        }
+
+        public void stop()
+        {
+            _speed = 0;
+        }
+
+        public void Accelerate()
+        {
+            _speed++;
+        }
+
+        public void Decrease()
+        {
+            if(_speed > 0)
+            {
+                _speed--;
+            }
+        }
+
+        public int GetSpeed()
+        {
+            return _speed;
+        }
+
+        public void SetSpeed(int speed)
+        {
+            _speed = speed;
+        }
+
+        
+    }
+}
